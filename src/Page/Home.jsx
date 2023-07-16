bc23e42ada351105750f056051de1e39dc623ae0
tanmoymandalofficial <tm.socialmidea@gmail.com>
2023-07-16 10:15:08 +0530

    error

--- src/Page/Home.jsx
+++ src/Page/Home.jsx
@@ -14,7 +14,7 @@ const Home = () => {
         <div className=' flex justify-center bg-cyan-500  font-bold '>
             <Link className=' py-3 px-6 text-white' to='/'>Home</Link>
             <Link className=' py-3 px-6' to='/about'>About</Link>
-            <Link className=' py-3 px-6' to='/contact'>Contect</Link>
+            <Link className=' py-3 px-6' to='/contact'>Contact</Link>
         </div>
         <div className=' container w-8/12 m-auto py-20 text-center'>
             <p className= ''>
